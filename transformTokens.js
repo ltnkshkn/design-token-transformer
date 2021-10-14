a67c97638fda431c90302068222a43b33d548fda
Merwan Boudriss <contact@mrwn.fr>
2021-10-14 18:19:39 +0200

    ✨Feat : Split scss files and create custom transform for shadow
    //Todo Explore way to use multiple shadow

--- transformTokens.js
+++ transformTokens.js
@@ -2,51 +2,46 @@ const StyleDictionary = require('style-dictionary')
 const baseConfig = require('./config.json')
 
 
+StyleDictionary.registerFilter({
+  name: 'isCategoryColor',
+  matcher: token => {
+    return token.attributes.category === 'color';
+  }
+});
+
+StyleDictionary.registerFilter({
+  name: 'isCategorySize',
+  matcher: token => {
+    return token.attributes.category === 'size';
+  }
+});
+
+StyleDictionary.registerFilter({
+  name: 'isCategoryEffect',
+  matcher: token => {
+     return token.attributes.category === 'effect';
+  }
+});
+
 StyleDictionary.registerTransform({
-  name: 'size/px',
+  name: 'effect/shadow',
   type: 'value',
   matcher: token => {
-    return token.unit === 'pixel' && token.value !== 0
+    return token.type === 'custom-shadow' && token.type !== 0
   },
   transformer: token => {
-    return `${token.value}px`
+    return `${token.value.offsetX}px ${token.value.offsetY}px ${token.value.spread}px ${token.value.radius}px ${token.value.color}`
   },
-})
-
-StyleDictionary.registerTransform({
-  name: 'size/percent',
-  type: 'value',
-  matcher: token => {
-    return token.unit === 'percent' && token.value !== 0
-  },
-  transformer: token => {
-    return `${token.value}%`
-  },
-})
-
-StyleDictionary.registerTransformGroup({
-  name: 'custom/css',
-  transforms: StyleDictionary.transformGroup['css'].concat([
-    'size/px',
-    'size/percent',
-  ]),
-})
-
-StyleDictionary.registerTransformGroup({
-  name: 'custom/less',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent',
-  ]),
 })
 
 StyleDictionary.registerTransformGroup({
   name: 'custom/scss',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent',
+  transforms: StyleDictionary.transformGroup['scss'].concat([
+    'effect/shadow',
+    'size/pxToRem'
   ]),
 })
+
 
 const StyleDictionaryExtended = StyleDictionary.extend(baseConfig)
 
